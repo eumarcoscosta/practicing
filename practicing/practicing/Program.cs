dd03ea07715a8b8010ebe68d96b64391c7e2b5f9
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-07-11 09:55:20 +0100

    Complete app

---
+++ practicing/practicing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int numero, i;
+
+        Console.WriteLine("Insira o numero abaixo: ");
+
+        numero = int.Parse(Console.ReadLine());
+
+        for (i = 1; i <= numero; i++)
+        {
+            int primeiroN = i;
+            int segundoN = i * i;
+            int terceiroN = i * i * i;
+
+            Console.WriteLine($"{primeiroN} {segundoN} {terceiroN}");
+        }
+
+    }
+}
